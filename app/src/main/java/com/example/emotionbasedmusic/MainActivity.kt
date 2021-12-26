4d2841809be77cb993f63228164dd60f1ae1b897
aryanO-o <aryandadhich.ad@gmail.com>
2021-12-26 13:31:19 +0530

    updated strings.xml dimen.xml and added emoji adapter, dataModel, data

--- app/src/main/java/com/example/emotionbasedmusic/MainActivity.kt
+++ app/src/main/java/com/example/emotionbasedmusic/MainActivity.kt
@@ -8,5 +8,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_face_proceed_or_retake)
 
+        //adding adapter on emoji list view of mood recognition fragment
+//        val emojiDataSet = emojiData().loadEmoji();
+//        val emojiRecyclerView = findViewById<RecyclerView>(R.id.emoji_recycler_view);
+//        emojiRecyclerView.adapter = emojiAdapter(this, emojiDataSet);
+
+
     }
 }
